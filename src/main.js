aa3963ae55739fb2879e9ee9346d4add3fef4d47
lubobo <173774418@qq.com>
2021-01-21 15:26:14 +0800

    修改代码顺序

--- src/main.js
+++ src/main.js
@@ -4,40 +4,32 @@ import router from './router'
 import store from './store'
 import axios from './utils/axios'
 import Antd from 'ant-design-vue';
+/** 缓存 */
 import Storage from 'vue-ls';
+/** 处理传参 */
 import qs from "qs";
 import VueI18n from 'vue-i18n'
 /* 定义全局日期过滤器 */
 import Moment from "moment";
 import "moment/locale/zh-cn";
-Moment.locale("zh-cn");
 /* 引入md5加密 */
 import md5 from "js-md5";
 /*引入mock */
 import "./mock/index.js";
-const options = {
-    namespace: '', // key prefix
-    name: 'ls', // name variable Vue.[ls] or this.[$ls],
-    storage: 'session', // storage name session, local, memory
-};
-Vue.use(Storage, options);
-Vue.use(Antd);
-Vue.use(VueI18n);
-axios.defaults.baseURL = "/fline/";
-Vue.prototype.$axios = axios
-Vue.prototype.$qs = qs;
-Vue.prototype.$md5 = md5;
-/** 可视化界面图表插件 **/
-import * as echarts from 'echarts'
-Vue.prototype.$echarts = echarts
 /*引入路由控制 */
 import './permission'
+/*api*/
+import api from '@/api/index'
 /*ie兼容es6*/
 import "babel-polyfill";
 /* 引入自己的css、less、js */
 import "@a/less/components.less";
 import "@a/less/common.less";
 import "@a/less/style.less";
+/* 引入全局组件 */
+import './utils/filter' // global filter
+/** 可视化界面图表插件 **/
+import * as echarts from 'echarts'
 /** 引入字体图标方式一：内网，无法访问互联网，需要下载图标代码 **/
 import "@a/font/iconfont.js";
 //  引入字体图标方式二：外网，可以访问互联网
@@ -45,12 +37,22 @@ import { Icon } from 'ant-design-vue';
 const IconFont = Icon.createFromIconfontCN({
 	scriptUrl: window.staticContentConfig.staticData.IconfontUrl,
 });
-Vue.component('IconFont', IconFont);
-/*api*/
-import api from '@/api/index'
+const options = {
+    namespace: '', // key prefix
+    name: 'ls', // name variable Vue.[ls] or this.[$ls],
+    storage: 'session', // storage name session, local, memory
+};
+Moment.locale("zh-cn");
+axios.defaults.baseURL = "/fline/";
+Vue.use(Storage, options);
+Vue.use(Antd);
+Vue.use(VueI18n);
+Vue.prototype.$axios = axios
+Vue.prototype.$qs = qs;
+Vue.prototype.$md5 = md5;
+Vue.prototype.$echarts = echarts
 Vue.prototype.$api = api
-/* 引入全局组件 */
-import './utils/filter' // global filter
+Vue.component('IconFont', IconFont);
 Vue.config.productionTip = false
 const i18n = new VueI18n({
     locale: 'zh-CN', // 语言标识, 通过切换locale的值来实现语言切换,this.$i18n.locale 
